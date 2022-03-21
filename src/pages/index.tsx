2604fb1c58f4a3605f1d930e84395cf69c4729b6
Henri Valette <henri.valette.fr@gmail.com>
2022-03-21 10:19:11 +0100

    chore: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="grid h-screen w-screen place-items-center">
+            Create an amazing React app
+        </div>
+    )
+}
+
+export default App
